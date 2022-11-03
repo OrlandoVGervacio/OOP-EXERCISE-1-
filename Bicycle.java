f54e4606b60ff5200bd21f7b7db902aab6266c1c
OrlandoVGervacio <116368279+OrlandoVGervacio@users.noreply.github.com>
2022-11-03 12:33:52 +0800

    Create Bicycle.java

---
+++ Bicycle.java
@@ -0,0 +1,24 @@
+public class Bike {
+	private String name, tagNo;
+	
+	public Bike() {
+		name = "unknown";
+		tagNo = "unknown";
+	}
+	
+	public String getTagNo() {
+		return tagNo;
+	}
+	
+	public void setTagNo(String tagNo) {
+		this.tagNo = tagNo;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}
